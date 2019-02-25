77648281d4916e69516ec9c04c0099d7dc807078
larsvers <larsverspohl@gmail.com>
2019-02-25 21:55:10 +0000

    Step 4

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+Vue.component('taco-card', {
+  data() {
+    return {
+      tagline: 'Think outside the bun',
+      count: 0,
+    };
+  },
+  props: {
+    emoji: String,
+  },
+  template: `
+    <div class="taco-card">
+      <div @click="changeCount">
+        {{ tagline }} and click me.
+      </div>
+      <div v-text="emoji"></div>
+      <div>You clicked me {{ count }} times. 
+        <span v-show="count > 5">Stop it!</span>
+      </div>
+    </div>
+  `,
+  methods: {
+    changeCount() {
+      this.count++;
+    },
+  },
+});
+
 const app = new Vue({
   el: '#container',
   data: {
     name: 'Taco Bell',
-    comicMode: false,
-    showName: true,
-    products: ['Tacos', 'Burritos', 'Nachos', 'More tacos'],
-  },
-  methods: {
-    changeComicMode() {
-      this.comicMode = !this.comicMode;
-    },
   },
 });
